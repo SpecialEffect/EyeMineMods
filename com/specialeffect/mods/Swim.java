adb26c69c50c6ca3191e6d33d22b0c0eaaef1fb7
Kirsty McNaught <kirsty_mcn@yahoo.com>
2016-03-29 15:40:30 +0100

    Add swimming (hold space) key binding.

---
+++ com/specialeffect/mods/Swim.java
@@ -0,0 +1,90 @@
+package com.specialeffect.mods;
+
+import java.util.Iterator;
+import java.util.Queue;
+
+import org.lwjgl.input.Keyboard;
+
+import com.specialeffect.callbacks.BaseClassWithCallbacks;
+import com.specialeffect.callbacks.IOnLiving;
+import com.specialeffect.callbacks.SingleShotOnLivingCallback;
+import com.specialeffect.utils.ModUtils;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.ChatComponentText;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.InputEvent;
+
+
+@Mod(modid = Swim.MODID, version = Swim.VERSION, name = Swim.NAME)
+
+public class Swim extends BaseClassWithCallbacks {
+
+	public static final String MODID = "specialeffect.swimtoggle";
+	public static final String VERSION = "0.1";
+	public static final String NAME = "SwimToggle";
+
+	private static KeyBinding mSwimKB;
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		FMLCommonHandler.instance().bus().register(this);
+
+		ModUtils.setupModInfo(event, this.MODID, this.VERSION, this.NAME,
+				"Add key binding to start/stop swimming (= jumping)");
+
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		// Subscribe to event buses
+		FMLCommonHandler.instance().bus().register(this);
+		MinecraftForge.EVENT_BUS.register(this);
+
+		// Register key bindings
+		mSwimKB = new KeyBinding("Toggle swimming", Keyboard.KEY_V, "SpecialEffect");
+		ClientRegistry.registerKeyBinding(mSwimKB);
+	}
+
+	@SubscribeEvent
+	public void onLiving(LivingUpdateEvent event) {
+		if (event.entityLiving instanceof EntityPlayer) {
+			this.processQueuedCallbacks(event);
+		}
+	}
+
+	@SubscribeEvent
+	public void onKeyInput(InputEvent.KeyInputEvent event) {
+		if(mSwimKB.isPressed()) {
+			final KeyBinding swimBinding = 
+					Minecraft.getMinecraft().gameSettings.keyBindJump;
+
+			if (swimBinding.isKeyDown()) {
+				KeyBinding.setKeyBindState(swimBinding.getKeyCode(), false);
+			}
+			else {
+				KeyBinding.setKeyBindState(swimBinding.getKeyCode(), true);
+			}
+			this.queueOnLivingCallback(new SingleShotOnLivingCallback(new IOnLiving()
+        	{				
+				@Override
+				public void onLiving(LivingUpdateEvent event) {
+					EntityPlayer player = (EntityPlayer)event.entityLiving;
+			        player.addChatComponentMessage(new ChatComponentText(
+			        		 "Swimming: " + (swimBinding.isKeyDown() ? "ON" : "OFF")));
+				}		
+			}));
+		}
+	}
+
+}
