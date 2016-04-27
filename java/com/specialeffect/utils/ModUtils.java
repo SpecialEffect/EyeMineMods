43163d4dcabf3106e97c2c029bc92df9ff9dbd59
Kirsty McNaught <kirsty_mcn@yahoo.com>
2016-04-27 11:19:08 +0100

    Add copyright headers to java files.

--- java/com/specialeffect/utils/ModUtils.java
+++ java/com/specialeffect/utils/ModUtils.java
@@ -1,3 +1,13 @@
+/**
+ * Copyright (C) 2016 Kirsty McNaught, SpecialEffect
+ * www.specialeffect.org.uk
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ */
+
 package com.specialeffect.utils;
 
 import java.awt.Point;
