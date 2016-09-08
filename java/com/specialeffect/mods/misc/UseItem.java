615b78b489f4dab06354109824369639d1ade313
Kirsty McNaught <kirsty_mcn@yahoo.com>
2016-09-08 12:51:31 +0100

    Add keybindings for using items.
    This will be necessary to support using OptiKey via
    mouse emulation rather than direct eyetracker support.

---
+++ java/com/specialeffect/mods/misc/UseItem.java
@@ -0,0 +1,119 @@
+/**
+ * Copyright (C) 2016 Kirsty McNaught, SpecialEffect
+ * www.specialeffect.org.uk
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ */
+
+package com.specialeffect.mods.misc;
+
+import java.awt.AWTException;
+import java.awt.event.KeyEvent;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
+
+import com.specialeffect.callbacks.BaseClassWithCallbacks;
+import com.specialeffect.callbacks.IOnLiving;
+import com.specialeffect.callbacks.SingleShotOnLivingCallback;
+import com.specialeffect.gui.StateOverlay;
+import com.specialeffect.mods.mining.ContinuouslyMine;
+import com.specialeffect.utils.ModUtils;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockDoor;
+import net.minecraft.block.BlockFenceGate;
+import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.state.BlockState;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.settings.GameSettings;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.util.Vec3;
+import net.minecraft.util.Vec3i;
+import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
+import net.minecraftforge.fml.client.FMLClientHandler;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.InputEvent;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
+import net.minecraftforge.fml.relauncher.Side;
+
+@Mod(modid = UseItem.MODID, version = ModUtils.VERSION, name = UseItem.NAME)
+public class UseItem extends BaseClassWithCallbacks {
+	public static final String MODID = "specialeffect.useItem";
+	public static final String NAME = "UseItem";
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		FMLCommonHandler.instance().bus().register(this);
+
+		ModUtils.setupModInfo(event, this.MODID, this.NAME, "Add key binding to use item without mouse");
+		ModUtils.setAsParent(event, SpecialEffectMisc.MODID);
+
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		// Subscribe to event buses
+		FMLCommonHandler.instance().bus().register(this);
+		MinecraftForge.EVENT_BUS.register(this);
+
+		// Register key bindings
+		mUseItemOnceKB = new KeyBinding("Use Item", Keyboard.KEY_NUMPAD0, "SpecialEffect");
+		ClientRegistry.registerKeyBinding(mUseItemOnceKB);
+
+		mUseItemContinuouslyKB = new KeyBinding("Use Item Continuously", Keyboard.KEY_NUMPAD1, "SpecialEffect");
+		ClientRegistry.registerKeyBinding(mUseItemContinuouslyKB);
+
+	}
+
+	private static KeyBinding mUseItemOnceKB;
+	private static KeyBinding mUseItemContinuouslyKB;
+
+
+	@SubscribeEvent
+	public void onLiving(LivingUpdateEvent event) {
+		if (event.entityLiving instanceof EntityPlayer) {
+			this.processQueuedCallbacks(event);
+		}
+	}
+
+	@SubscribeEvent
+	public void onKeyInput(InputEvent.KeyInputEvent event) {
+		
+		final KeyBinding useItemKeyBinding = Minecraft.getMinecraft().gameSettings.keyBindUseItem;
+				
+		if (mUseItemContinuouslyKB.isPressed()) {
+			boolean useItemNewState = !useItemKeyBinding.isKeyDown();
+			KeyBinding.setKeyBindState(useItemKeyBinding.getKeyCode(), useItemNewState);			
+			this.queueChatMessage("Using item: " + (useItemNewState ? "ON" : "OFF"));
+		}
+		else if (mUseItemOnceKB.isPressed()) {
+			KeyBinding.onTick(useItemKeyBinding.getKeyCode());
+		}
+	}
+}
