c54c0b88b70f412794d1b770fbc8151b4f380546
Kirsty McNaught <kirsty_mcn@yahoo.com>
2017-06-30 19:06:57 +0100

    WIP pillar

--- java/com/specialeffect/mods/misc/AutoPillar.java
+++ java/com/specialeffect/mods/misc/AutoPillar.java
@@ -21,6 +21,7 @@ import com.specialeffect.utils.KeyPressCounter;
 import com.specialeffect.utils.ModUtils;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiChat;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
@@ -41,149 +42,149 @@ import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import net.minecraftforge.fml.relauncher.Side;
 
-@Mod(modid = AutoPillar.MODID, 
-	 version = ModUtils.VERSION,
-	 name = AutoPillar.NAME)
-public class AutoPillar extends BaseClassWithCallbacks
-{
-    public static final String MODID = "specialeffect.autopillar";
-    public static final String NAME = "AutoPillar";
+@Mod(modid = AutoPillar.MODID, version = ModUtils.VERSION, name = AutoPillar.NAME)
+public class AutoPillar extends BaseClassWithCallbacks {
+	public static final String MODID = "specialeffect.autopillar";
+	public static final String NAME = "AutoPillar";
 
-    public static KeyBinding autoPlaceKeyBinding;
-    
-    public static SimpleNetworkWrapper network;
-    
-    private KeyPressCounter keyCounterWalkDir = new KeyPressCounter();
+	public static KeyBinding autoPlaceKeyBinding;
+	public static KeyBinding openChatKB;
 
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event) {    
-    	FMLCommonHandler.instance().bus().register(this);
-        
-        ModUtils.setupModInfo(event, this.MODID, this.NAME,
-				"Add key binding to create pillar, or 'nerd-pole'.");
+	public static SimpleNetworkWrapper network;
+
+	private KeyPressCounter keyCounterWalkDir = new KeyPressCounter();
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		FMLCommonHandler.instance().bus().register(this);
+
+		ModUtils.setupModInfo(event, this.MODID, this.NAME, "Add key binding to create pillar, or 'nerd-pole'.");
 		ModUtils.setAsParent(event, SpecialEffectMisc.MODID);
 
-        network = NetworkRegistry.INSTANCE.newSimpleChannel(this.NAME);
-        network.registerMessage(UseItemAtPositionMessage.Handler.class, UseItemAtPositionMessage.class, 0, Side.SERVER);
-    }
-    
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-		// Subscribe to event buses
-        FMLCommonHandler.instance().bus().register(this);
-    	MinecraftForge.EVENT_BUS.register(this);
+		network = NetworkRegistry.INSTANCE.newSimpleChannel(this.NAME);
+		network.registerMessage(UseItemAtPositionMessage.Handler.class, UseItemAtPositionMessage.class, 0, Side.SERVER);
+	}
 
-    	// Register key bindings
-        autoPlaceKeyBinding = new KeyBinding("Auto-place block", Keyboard.KEY_L, "SpecialEffect");
-        ClientRegistry.registerKeyBinding(autoPlaceKeyBinding);
-        
-    }
-    
-    private float lastPlayerPitch;
-    
-    @SubscribeEvent
-    public void onLiving(LivingUpdateEvent event) {
-    	if (ModUtils.entityIsMe(event.getEntityLiving())) {
-			EntityPlayer player = (EntityPlayer)event.getEntityLiving();
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		// Subscribe to event buses
+		FMLCommonHandler.instance().bus().register(this);
+		MinecraftForge.EVENT_BUS.register(this);
+
+		// Register key bindings
+		autoPlaceKeyBinding = new KeyBinding("Auto-place block", Keyboard.KEY_L, "SpecialEffect");
+		ClientRegistry.registerKeyBinding(autoPlaceKeyBinding);
+
+		openChatKB = new KeyBinding("open the chat blah", Keyboard.KEY_PERIOD, "SpecialEffect");
+		ClientRegistry.registerKeyBinding(openChatKB);
+
+		// Minecraft.getMinecraft().gameSettings.keyBindChat
+	}
+
+	private float lastPlayerPitch;
+
+	@SubscribeEvent
+	public void onLiving(LivingUpdateEvent event) {
+		if (ModUtils.entityIsMe(event.getEntityLiving())) {
+			EntityPlayer player = (EntityPlayer) event.getEntityLiving();
 			synchronized (mOnLivingQueue) {
 				this.lastPlayerPitch = player.rotationPitch;
 			}
-    				
-    		// Process any events which were queued by key events
-    		this.processQueuedCallbacks(event);
-    	}
-    }
-    
-    @SubscribeEvent
-    public void onKeyInput(InputEvent.KeyInputEvent event) {
-        
-        // Auto place is implemented as:
-        // - Next onLiving tick: look at floor, jump
-        // - After 10 ticks (when jump is high): place current item below player.
-    	// - From 12 - 17ticks, gradually reset view 
-    	// Technically there is no need to change player's view, but the user experience
-    	// is weird if you don't (you don't really know what just happened).
-        if(autoPlaceKeyBinding.isPressed()) {   
-        	float origPitchTemp = 0;        	
-        	synchronized (mOnLivingQueue) {
-        		origPitchTemp = this.lastPlayerPitch;
+
+			// Process any events which were queued by key events
+			this.processQueuedCallbacks(event);
+		}
+	}
+
+	@SubscribeEvent
+	public void onKeyInput(InputEvent.KeyInputEvent event) {
+
+		// Auto place is implemented as:
+		// - Next onLiving tick: look at floor, jump
+		// - After 10 ticks (when jump is high): place current item below
+		// player.
+		// - From 12 - 17ticks, gradually reset view
+		// Technically there is no need to change player's view, but the user
+		// experience
+		// is weird if you don't (you don't really know what just happened).
+		if (autoPlaceKeyBinding.isPressed()) {
+			float origPitchTemp = 0;
+			synchronized (mOnLivingQueue) {
+				origPitchTemp = this.lastPlayerPitch;
 			}
-  			final float origPitch = origPitchTemp;
-  			final float pillarPitch = 85; // 90 = look straight down
-  			final int numTicksReset = 5;
-  		
-  			final float deltaPitch = (pillarPitch - origPitch)/numTicksReset;
-  			
-        	this.queueOnLivingCallback(new SingleShotOnLivingCallback(new IOnLiving()
-        	{				
+			final float origPitch = origPitchTemp;
+			final float pillarPitch = 85; // 90 = look straight down
+			final int numTicksReset = 5;
+
+			final float deltaPitch = (pillarPitch - origPitch) / numTicksReset;
+
+			this.queueOnLivingCallback(new SingleShotOnLivingCallback(new IOnLiving() {
 				@Override
 				public void onLiving(LivingUpdateEvent event) {
-		            // It's important to make sure we're approximately - but not exactly - centred
-		            // on a block here, so that the block always ends up under us (if you do this
-		            // with integer positions you often find your position alternating between 2 blocks)
-		    		// Also look down, purely for effect.
-					EntityPlayer player = (EntityPlayer)event.getEntityLiving();
-					player.setPositionAndRotation(Math.floor(player.posX)+0.4, 
-		    						   Math.floor(player.posY), 
-		    						   Math.floor(player.posZ)+0.4,
-		    						   player.rotationYaw,
-		    						   pillarPitch);
-
-		    		// Then jump
-		    		player.jump();
-				}		
+					// It's important to make sure we're approximately - but not
+					// exactly - centred
+					// on a block here, so that the block always ends up under
+					// us (if you do this
+					// with integer positions you often find your position
+					// alternating between 2 blocks)
+					// Also look down, purely for effect.
+					EntityPlayer player = (EntityPlayer) event.getEntityLiving();
+					player.setPositionAndRotation(Math.floor(player.posX) + 0.4, Math.floor(player.posY),
+							Math.floor(player.posZ) + 0.4, player.rotationYaw, pillarPitch);
+					System.out.println("jump movement factor: " + player.jumpMovementFactor);
+					System.out.println("posY before jump: " + player.posY);
+					// Then jump
+					player.jump();
+				}
 			}));
-        	this.queueOnLivingCallback(new DelayedOnLivingCallback(new IOnLiving()
-        	{				
+			this.queueOnLivingCallback(new DelayedOnLivingCallback(new IOnLiving() {
 				@Override
 				public void onLiving(LivingUpdateEvent event) {
- 	                World world = Minecraft.getMinecraft().world;
-		    		EntityPlayer player = (EntityPlayer)event.getEntityLiving();
-		    		BlockPos playerPos = player.getPosition();
+					World world = Minecraft.getMinecraft().world;
+					EntityPlayer player = (EntityPlayer) event.getEntityLiving();
+					BlockPos playerPos = player.getPosition();
 
-	                ItemStack item = player.getHeldItem(EnumHand.MAIN_HAND);
-	                if (item != null) {
-	                	BlockPos blockPos = new BlockPos(playerPos.getX(), 
-	                									 playerPos.getY()-1, // y = up 
-	                									 playerPos.getZ());
-	                	
-			            // Ask server to use item
-			    		AutoPillar.network.sendToServer(
-			    				new UseItemAtPositionMessage(item, blockPos));
+					ItemStack item = player.getHeldItem(EnumHand.MAIN_HAND);
+					System.out.println("posY when placing item: " + player.posY);
 
-			    		// Make sure we get the animation
-			    		player.swingArm(EnumHand.MAIN_HAND);
-	                }
-				}		
-			},
-        	10 )); // delayed by 10 ticks
-        	
-        	for (int i = 1; i < numTicksReset+1; i++) {        		
-        		final int j = i;
-        		this.queueOnLivingCallback(new DelayedOnLivingCallback(new IOnLiving()
-	        	{				
+					if (item != null) {
+						BlockPos blockPos = new BlockPos(playerPos.getX(), playerPos.getY() - 1, // y
+																									// =
+																									// up
+								playerPos.getZ());
+
+						// Ask server to use item
+						AutoPillar.network.sendToServer(new UseItemAtPositionMessage(item, blockPos));
+
+						// Make sure we get the animation
+						player.swingArm(EnumHand.MAIN_HAND);
+					}
+				}
+			}, 10)); // delayed by 10 ticks
+
+			for (int i = 1; i < numTicksReset + 1; i++) {
+				final int j = i;
+				this.queueOnLivingCallback(new DelayedOnLivingCallback(new IOnLiving() {
 					@Override
 					public void onLiving(LivingUpdateEvent event) {
-						EntityPlayer player = (EntityPlayer)event.getEntityLiving();
-						
-						player.setPositionAndRotation(player.posX,
-			    						  		 	  player.posY, 
-			    						  		 	  player.posZ,
-						    						  player.rotationYaw,
-						    						  pillarPitch - deltaPitch*j);
-						// This forces an update, otherwise you sometimes lose the
-						// last delta (maybe because we're just updating locally)
-						player.setPositionAndUpdate(player.posX,
-			    						  		 	  player.posY, 
-			    						  		 	  player.posZ);
-					}		
-				},
-	        	12+j));
-        	}
-        }
-    }
-    
-    
-    
+						EntityPlayer player = (EntityPlayer) event.getEntityLiving();
+
+						player.setPositionAndRotation(player.posX, player.posY, player.posZ, player.rotationYaw,
+								pillarPitch - deltaPitch * j);
+						// This forces an update, otherwise you sometimes lose
+						// the
+						// last delta (maybe because we're just updating
+						// locally)
+						player.setPositionAndUpdate(player.posX, player.posY, player.posZ);
+					}
+				}, 12 + j));
+			}
+		}
+
+		if (openChatKB.isPressed()) {
+			Minecraft.getMinecraft().displayGuiScreen(new GuiChat());
+		}
+
+	}
+
 }
