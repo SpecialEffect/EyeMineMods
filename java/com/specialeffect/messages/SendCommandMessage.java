15c030f6123c6d889613214e8bfd6a8c2dd0d702
Kirsty McNaught <kirsty_mcn@yahoo.com>
2017-07-15 21:58:06 +0100

    Add mod for a couple of commands

---
+++ java/com/specialeffect/messages/SendCommandMessage.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (C) 2016 Kirsty McNaught, SpecialEffect
+ * www.specialeffect.org.uk
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ */
+
+package com.specialeffect.messages;
+import java.io.UnsupportedEncodingException;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
+import net.minecraft.client.Minecraft;
+import net.minecraft.command.ICommandManager;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.IThreadListener;
+import net.minecraft.world.GameRules;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
+import net.minecraftforge.fml.common.network.ByteBufUtils;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+import scala.Console;
+
+public class SendCommandMessage implements IMessage {
+    private String command = "";
+
+    public SendCommandMessage() { }
+    
+    public SendCommandMessage(String cmd) {
+        this.command = cmd;
+    }
+
+    @Override
+    public void fromBytes(ByteBuf buf) {
+    	command = ByteBufUtils.readUTF8String(buf);
+    }
+    
+    @Override
+    public void toBytes(ByteBuf buf) {
+    	ByteBufUtils.writeUTF8String(buf, command);
+    }
+
+    public static class Handler implements IMessageHandler<SendCommandMessage, IMessage> {        
+    	@Override
+        public IMessage onMessage(final SendCommandMessage message,final MessageContext ctx) {
+            IThreadListener mainThread = (WorldServer) ctx.getServerHandler().playerEntity.world; // or Minecraft.getMinecraft() on the client
+            mainThread.addScheduledTask(new Runnable() {
+                @Override
+                public void run() {
+
+                	MinecraftServer server;
+                	server = Minecraft.getMinecraft().world.getMinecraftServer();
+                	if (null == server) { // this is for non-network worlds
+                		server = Minecraft.getMinecraft().getIntegratedServer();
+                	}
+                	if (null == server) {
+                		System.out.println("Server is null, cannot send command");
+                		return;
+                	}
+                	else {
+                		ICommandManager mgr = server.getCommandManager();
+                		if (null == mgr) {
+                    		System.out.println("CommandManager is null, cannot send command");
+                			return;
+                		}
+                		else {
+                			mgr.executeCommand(server, message.command);
+                		}
+                	}
+                }
+            });
+            return null; // no response in this case
+        }
+    }
+}
+
+
+
---
+++ java/com/specialeffect/mods/misc/QuickCommands.java
@@ -0,0 +1,137 @@
+/**
+ * Copyright (C) 2016 Kirsty McNaught, SpecialEffect
+ * www.specialeffect.org.uk
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ */
+
+package com.specialeffect.mods.misc;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.lwjgl.input.Keyboard;
+
+import com.specialeffect.callbacks.BaseClassWithCallbacks;
+import com.specialeffect.callbacks.IOnLiving;
+import com.specialeffect.callbacks.OnLivingCallback;
+import com.specialeffect.callbacks.SingleShotOnLivingCallback;
+import com.specialeffect.messages.SendCommandMessage;
+import com.specialeffect.utils.ModUtils;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntityPotion;
+import net.minecraft.init.Items;
+import net.minecraft.init.MobEffects;
+import net.minecraft.init.PotionTypes;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.potion.PotionType;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.GameRules;
+import net.minecraft.world.World;
+import net.minecraft.world.storage.WorldInfo;
+import net.minecraftforge.client.ClientCommandHandler;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.InputEvent;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
+import net.minecraftforge.fml.relauncher.Side;
+
+@Mod(modid = QuickCommands.MODID, version = ModUtils.VERSION, name = QuickCommands.NAME)
+public class QuickCommands extends BaseClassWithCallbacks {
+	public static final String MODID = "specialeffect.quickcommands";
+	public static final String NAME = "QuickCommands";
+    public static SimpleNetworkWrapper network;
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		FMLCommonHandler.instance().bus().register(this);
+
+		ModUtils.setupModInfo(event, this.MODID, this.NAME, "Add key bindings for some useful commands.");
+		ModUtils.setAsParent(event, SpecialEffectMisc.MODID);
+		
+		// Register for server messages
+		network = NetworkRegistry.INSTANCE.newSimpleChannel(this.NAME);
+        network.registerMessage(SendCommandMessage.Handler.class, 
+        						SendCommandMessage.class, 0, Side.SERVER);
+
+
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		// Subscribe to event buses
+		FMLCommonHandler.instance().bus().register(this);
+		MinecraftForge.EVENT_BUS.register(this);
+
+		// Register key bindings
+		mNightVisionKB = new KeyBinding("Toggle night vision", Keyboard.KEY_F13, "SpecialEffect");
+		ClientRegistry.registerKeyBinding(mNightVisionKB);
+
+		mDayNightKB = new KeyBinding("Toggle day/night cycle", Keyboard.KEY_F14, "SpecialEffect");
+		ClientRegistry.registerKeyBinding(mDayNightKB);
+
+	}
+
+	private static KeyBinding mNightVisionKB;
+	private static KeyBinding mDayNightKB;
+
+	@SubscribeEvent
+	public void onLiving(LivingUpdateEvent event) {
+		if (ModUtils.entityIsMe(event.getEntityLiving())) {
+			this.processQueuedCallbacks(event);
+		}
+	}
+
+	@SubscribeEvent
+	public void onKeyInput(InputEvent.KeyInputEvent event) {
+		if (mNightVisionKB.isPressed()) {
+			this.queueOnLivingCallback(new SingleShotOnLivingCallback(new IOnLiving() {
+				@Override
+				public void onLiving(LivingUpdateEvent event) {
+					if (ModUtils.entityIsMe(event.getEntityLiving())) {
+						EntityPlayer player = (EntityPlayer) event.getEntityLiving();
+						Potion nightVision = MobEffects.NIGHT_VISION;
+						if (player.isPotionActive(nightVision)) {
+							System.out.println("clearing");
+							player.removePotionEffect(nightVision);
+						}
+						else {
+							System.out.println("night vision");
+							player.addPotionEffect(new PotionEffect(nightVision));
+						}
+					}
+				}
+			}));
+		}
+		if (mDayNightKB.isPressed()) {
+			this.queueOnLivingCallback(new SingleShotOnLivingCallback(new IOnLiving() {
+				
+				@Override
+				public void onLiving(LivingUpdateEvent event) {
+					String gameRule = "doDaylightCycle";
+					GameRules rules = Minecraft.getMinecraft().world.getGameRules();	
+					boolean newBool = !rules.getBoolean(gameRule);
+					
+					// Ask server to change gamerule
+					String cmd = "/gamerule " + gameRule + " " + Boolean.toString(newBool);
+					QuickCommands.network.sendToServer(new SendCommandMessage(cmd));
+				}
+			}));			
+		}
+	}
+}
