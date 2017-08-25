b6e82d6a1c232803524fa340f412bad88ae38f1f
Kirsty McNaught <kirsty_mcn@yahoo.com>
2017-08-25 17:19:39 +0100

    Add mod to swap mine/place key bindings
    This might be useful for people who want to use a switch
    for mining/placing. They can configure the one switch to
    do (e.g.) placing, and then with a single command toggle
    to mining (or vice versa).

---
+++ java/com/specialeffect/mods/misc/SwapMinePlace.java
@@ -0,0 +1,104 @@
+/**
+ * Copyright (C) 2016 Kirsty McNaught, SpecialEffect
+ * www.specialeffect.org.uk
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ */
+
+package com.specialeffect.mods.misc;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.lwjgl.input.Keyboard;
+
+import com.specialeffect.callbacks.BaseClassWithCallbacks;
+import com.specialeffect.callbacks.IOnLiving;
+import com.specialeffect.callbacks.OnLivingCallback;
+import com.specialeffect.callbacks.SingleShotOnLivingCallback;
+import com.specialeffect.messages.SendCommandMessage;
+import com.specialeffect.utils.ModUtils;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntityPotion;
+import net.minecraft.init.Items;
+import net.minecraft.init.MobEffects;
+import net.minecraft.init.PotionTypes;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.potion.PotionType;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.GameRules;
+import net.minecraft.world.World;
+import net.minecraft.world.storage.WorldInfo;
+import net.minecraftforge.client.ClientCommandHandler;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.InputEvent;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
+import net.minecraftforge.fml.relauncher.Side;
+
+@Mod(modid = SwapMinePlace.MODID, version = ModUtils.VERSION, name = SwapMinePlace.NAME)
+public class SwapMinePlace extends BaseClassWithCallbacks {
+	public static final String MODID = "specialeffect.swapmineplace";
+	public static final String NAME = "SwapMinePlace";
+	public static SimpleNetworkWrapper network;
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		FMLCommonHandler.instance().bus().register(this);
+
+		ModUtils.setupModInfo(event, this.MODID, this.NAME, "Add key binding to swap mine/place key bindings.");
+		ModUtils.setAsParent(event, SpecialEffectMisc.MODID);
+
+		// Register for server messages
+		network = NetworkRegistry.INSTANCE.newSimpleChannel(this.NAME);
+		network.registerMessage(SendCommandMessage.Handler.class, SendCommandMessage.class, 0, Side.SERVER);
+
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		// Subscribe to event buses
+		FMLCommonHandler.instance().bus().register(this);
+		MinecraftForge.EVENT_BUS.register(this);
+
+		// Register key bindings
+		mSwapKB = new KeyBinding("Swap mine/place keys", Keyboard.KEY_F11, "SpecialEffect");
+		ClientRegistry.registerKeyBinding(mSwapKB);
+
+	}
+	
+	@SubscribeEvent
+	public void onLiving(LivingUpdateEvent event) {
+		if (ModUtils.entityIsMe(event.getEntityLiving())) {
+			this.processQueuedCallbacks(event);
+		}
+	}
+
+	private static KeyBinding mSwapKB;
+
+	@SubscribeEvent
+	public void onKeyInput(InputEvent.KeyInputEvent event) {
+		if (mSwapKB.isPressed()) {
+			KeyBinding kbAttack = Minecraft.getMinecraft().gameSettings.keyBindAttack;
+			KeyBinding kbPlace = Minecraft.getMinecraft().gameSettings.keyBindUseItem;
+			Minecraft.getMinecraft().gameSettings.keyBindAttack = kbPlace;
+			Minecraft.getMinecraft().gameSettings.keyBindUseItem = kbAttack;			
+			this.queueChatMessage("Swapping mine and place keys");			
+		}
+	}
+}
