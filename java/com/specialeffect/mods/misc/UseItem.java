f010c4ae526e3a8fbb827acf09896ceb6909a352
K McNaught <kmcnaught.git@gmail.com>
2018-01-19 11:40:55 +0000

    Add keybindings for prev/next item
    This allows the actions to be used from optikey
    when using mouse emulation as input.

--- java/com/specialeffect/mods/misc/UseItem.java
+++ java/com/specialeffect/mods/misc/UseItem.java
@@ -13,12 +13,15 @@ package com.specialeffect.mods.misc;
 import org.lwjgl.input.Keyboard;
 
 import com.specialeffect.callbacks.BaseClassWithCallbacks;
+import com.specialeffect.callbacks.IOnLiving;
+import com.specialeffect.callbacks.SingleShotOnLivingCallback;
 import com.specialeffect.mods.EyeGaze;
 import com.specialeffect.utils.CommonStrings;
 import com.specialeffect.utils.ModUtils;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
@@ -57,11 +60,18 @@ public class UseItem extends BaseClassWithCallbacks {
 		mUseItemContinuouslyKB = new KeyBinding("Use item continuously", Keyboard.KEY_NUMPAD1, CommonStrings.EYEGAZE_EXTRA);
 		ClientRegistry.registerKeyBinding(mUseItemContinuouslyKB);
 
+		mPrevItemKB = new KeyBinding("Select previous item", Keyboard.KEY_NUMPAD4, CommonStrings.EYEGAZE_EXTRA);
+		ClientRegistry.registerKeyBinding(mPrevItemKB);
+
+		mNextItemKB = new KeyBinding("Select next item", Keyboard.KEY_NUMPAD5, CommonStrings.EYEGAZE_EXTRA);
+		ClientRegistry.registerKeyBinding(mNextItemKB);
+
 	}
 
 	private static KeyBinding mUseItemOnceKB;
 	private static KeyBinding mUseItemContinuouslyKB;
-
+	private static KeyBinding mPrevItemKB;
+	private static KeyBinding mNextItemKB;
 
 	@SubscribeEvent
 	public void onLiving(LivingUpdateEvent event) {
@@ -82,6 +92,24 @@ public class UseItem extends BaseClassWithCallbacks {
 		}
 		else if (mUseItemOnceKB.isPressed()) {
 			KeyBinding.onTick(useItemKeyBinding.getKeyCode());
+		}
+		else if (mPrevItemKB.isPressed()) {
+			this.queueOnLivingCallback(new SingleShotOnLivingCallback(new IOnLiving() {				
+				@Override
+				public void onLiving(LivingUpdateEvent event) {
+					EntityPlayer player = (EntityPlayer) event.getEntityLiving();
+					player.inventory.changeCurrentItem(1);
+				}
+			}));
+		}
+		else if (mNextItemKB.isPressed()) {
+			this.queueOnLivingCallback(new SingleShotOnLivingCallback(new IOnLiving() {				
+				@Override
+				public void onLiving(LivingUpdateEvent event) {
+					EntityPlayer player = (EntityPlayer) event.getEntityLiving();
+					player.inventory.changeCurrentItem(-1);
+				}
+			}));
 		}
 	}
 }
