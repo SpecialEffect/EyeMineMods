bc003bb014f6e637d8635b41f176c84f1a6a14b7
K McNaught <kmcnaught.git@gmail.com>
2018-03-05 15:24:05 +0000

    Update version in gradle for correct filename

--- update_version.py
+++ update_version.py
@@ -9,14 +9,14 @@ import os
 import sys
 import getopt
 
-if len(sys.argv) < 2:
-    print("usage: ./update_version.py --major")
-    print("or:    ./update_version.py --minor")
-    print("or:    ./update_version.py --revision")
+if len(sys.argv) < 3:
+    print("usage: ./update_version.py <forge-source-dir> --[major/minor/revision]")
+    print("e.g:   ./update_version.py ../forge-1.8.8-11.15.0.1608-1.8.8-mdk --revision")
     sys.exit(1)
 
 origPath = os.getcwd();
-version_level = sys.argv[1];
+forge_dir = sys.argv[1]
+version_level = sys.argv[2];
 valid_version_levels = ['--major', '--minor', '--revision']
 if version_level not in valid_version_levels:
     print('Version level "{}" not recognised, must be one of:'.format(version_level))
@@ -65,16 +65,35 @@ def updateModVersion(filename, version_level):
     if not new_version:
         raise Exception("Could not find version in file {}".format(filename))
     return new_version;        
-        
+  
+def updateVersionGradle(filename, new_version):
+    # This updates the version in build.gradle. This file is 
+    # not within our repo, so beware!
+    
+    #e.g. version = "1.0.3"
+    pattern = re.compile('version\s*=\s*"(\d*.\d*.\d*)"');
+
+    for line in fileinput.input(filename, inplace=True):
+        if re.search(pattern, line): 
+            curr_version = pattern.search(line).groups()[0]
+            line = line.replace(curr_version, new_version)    
+        print(line.rstrip('\n'))
+    return new_version
+    
+    
 # Don't continue if working copy is dirty
-if not safeProcess('git diff-index --quiet HEAD --'):
-    print( "Cannot continue, git working copy dirty")
-    safeExit()
+#if not safeProcess('git diff-index --quiet HEAD --'):
+#    print( "Cannot continue, git working copy dirty")
+#    safeExit()
     
 # Make sure the mod reports the new version.
 version_file = "java/com/specialeffect/utils/ModUtils.java"
 new_version = updateModVersion(version_file, version_level);
 print(new_version)
+
+# Make sure gradle knows about the version
+gradle_file = '{}/build.gradle'.format(forge_dir)
+updateVersionGradle(gradle_file, new_version)
 
 # Commit changes
 safeProcess("git add {}".format(version_file))
