18d81e823c71445d9ec5236ddf261859bebf915b
K McNaught <kirsty.mcnaught@gmail.com>
2020-01-07 17:12:49 +0000

    Fix UseItem
    - immediately grab player and update in onKeyInput, don't
    save for an onLiving tick
    - This fixes the synchronisation issues that were sometimes
    occurring

--- src/main/java/com/specialeffect/mods/misc/UseItem.java
+++ src/main/java/com/specialeffect/mods/misc/UseItem.java
@@ -15,6 +15,7 @@ import org.lwjgl.glfw.GLFW;
 import com.specialeffect.callbacks.BaseClassWithCallbacks;
 import com.specialeffect.callbacks.IOnLiving;
 import com.specialeffect.callbacks.SingleShotOnLivingCallback;
+import com.specialeffect.messages.ActivateBlockAtPosition;
 import com.specialeffect.mods.EyeGaze;
 import com.specialeffect.utils.CommonStrings;
 import com.specialeffect.utils.ModUtils;
@@ -23,6 +24,8 @@ import net.java.games.input.Keyboard;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.client.event.InputEvent.KeyInputEvent;
 import net.minecraftforge.common.MinecraftForge;
@@ -34,30 +37,34 @@ import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.network.NetworkRegistry;
 
 @Mod(UseItem.MODID)
-public class UseItem extends BaseClassWithCallbacks {
+public class UseItem {
 	public static final String MODID = "useitem";
 	public static final String NAME = "UseItem";
+	private static final String PROTOCOL_VERSION = Integer.toString(1);
 
 	public UseItem() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
 	}
-	
+
 	private void setup(final FMLCommonSetupEvent event) {
-		
+
 		MinecraftForge.EVENT_BUS.register(this);
 
 		ModUtils.setupModInfo(event, this.MODID, this.NAME, "Add key binding to use item without mouse");
 		ModUtils.setAsParent(event, EyeGaze.MODID);
 
 		// FMLInitializationEvent
+		
 
 		// Register key bindings
 		mUseItemOnceKB = new KeyBinding("Use item", GLFW.GLFW_KEY_KP_0, CommonStrings.EYEGAZE_COMMON);
 		ClientRegistry.registerKeyBinding(mUseItemOnceKB);
 
-		mUseItemContinuouslyKB = new KeyBinding("Use item continuously", GLFW.GLFW_KEY_KP_1, CommonStrings.EYEGAZE_EXTRA);
+		mUseItemContinuouslyKB = new KeyBinding("Use item continuously", GLFW.GLFW_KEY_KP_1,
+				CommonStrings.EYEGAZE_EXTRA);
 		ClientRegistry.registerKeyBinding(mUseItemContinuouslyKB);
 
 		mPrevItemKB = new KeyBinding("Select previous item", GLFW.GLFW_KEY_KP_4, CommonStrings.EYEGAZE_EXTRA);
@@ -74,46 +81,22 @@ public class UseItem extends BaseClassWithCallbacks {
 	private static KeyBinding mNextItemKB;
 
 	@SubscribeEvent
-	public void onLiving(LivingUpdateEvent event) {
-		if (ModUtils.entityIsMe(event.getEntityLiving())) {
-			this.processQueuedCallbacks(event);
-		}
-	}
-	
-	@SubscribeEvent
 	public void onKeyInput(KeyInputEvent event) {
-        
+
 		final KeyBinding useItemKeyBinding = Minecraft.getInstance().gameSettings.keyBindUseItem;
-				
+		PlayerEntity player = Minecraft.getInstance().player;
+		
 		if (mUseItemContinuouslyKB.isPressed()) {
 			boolean useItemNewState = !useItemKeyBinding.isKeyDown();
-			KeyBinding.setKeyBindState(useItemKeyBinding.getKey(), useItemNewState);			
-			this.queueChatMessage("Using item: " + (useItemNewState ? "ON" : "OFF"));
-		}
-		else if (mUseItemOnceKB.isPressed()) {
+			KeyBinding.setKeyBindState(useItemKeyBinding.getKey(), useItemNewState);
+			final String message = "Using item: " + (useItemNewState ? "ON" : "OFF");
+	        player.sendMessage(new StringTextComponent(message));
+		} else if (mUseItemOnceKB.isPressed()) {
 			KeyBinding.onTick(useItemKeyBinding.getKey());
-		}
-		else if (mPrevItemKB.isPressed()) {
-			this.queueOnLivingCallback(new SingleShotOnLivingCallback(new IOnLiving() {				
-				@Override
-				public void onLiving(LivingUpdateEvent event) {
-					PlayerEntity player = (PlayerEntity) event.getEntityLiving();
-					player.inventory.changeCurrentItem(1);
-				}
-			}));
-		}
-		else if (mNextItemKB.isPressed()) {
-			this.queueOnLivingCallback(new SingleShotOnLivingCallback(new IOnLiving() {				
-				@Override
-				public void onLiving(LivingUpdateEvent event) {
-					PlayerEntity player = (PlayerEntity) event.getEntityLiving();
-					player.inventory.changeCurrentItem(-1);
-					
-					// TODO: need to tell server about this too??
-					// about 25% of the time, there's a mismatch between what you see in 
-					// hotbar, and what gets used (with a glitch sometimes when you place).
-				}
-			}));
+		} else if (mPrevItemKB.isPressed()) {
+			player.inventory.changeCurrentItem(1);
+		} else if (mNextItemKB.isPressed()) {
+			player.inventory.changeCurrentItem(-1);
 		}
 	}
 }
