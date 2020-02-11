36211bc4ab15a7da7446b1a68462cf2f9696bb5d
K McNaught <kirsty.mcnaught@gmail.com>
2020-02-11 11:28:53 +0000

    Add channel setup in base mod

--- src/main/java/com/specialeffect/mods/ChildMod.java
+++ src/main/java/com/specialeffect/mods/ChildMod.java
@@ -3,17 +3,33 @@ package com.specialeffect.mods;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.network.NetworkRegistry;
+import net.minecraftforge.fml.network.simple.SimpleChannel;
 
 public class ChildMod {
 	
     // Directly reference a log4j logger.	
     @SuppressWarnings("unused")
 	public static final Logger LOGGER = LogManager.getLogger();
+    protected SimpleChannel channel;
 
 	public void setup(final FMLCommonSetupEvent event) {
 		// can be overridden if mod has setup to do
 	}
+	
+	protected void setupChannel(String modid, int protocolVersion) {
+		String PROTOCOL_VERSION = Integer.toString(protocolVersion);
+
+		channel = NetworkRegistry.newSimpleChannel(
+                new ResourceLocation("specialeffect", modid)
+                ,() -> PROTOCOL_VERSION
+                , PROTOCOL_VERSION::equals
+                , PROTOCOL_VERSION::equals);
+
+	}
+	
 //	public void onKeyInput(KeyInputEvent event);    	
 //	public void onLiving(LivingUpdateEvent event);
 //	public void updateIcons();
