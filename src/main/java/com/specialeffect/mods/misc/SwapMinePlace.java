8920c5c8022621b3494b88dc0ac38643b2826b98
K McNaught <kirsty.mcnaught@gmail.com>
2020-02-27 16:04:28 +0000

    State message when mine/place are swapped

--- src/main/java/com/specialeffect/mods/misc/SwapMinePlace.java
+++ src/main/java/com/specialeffect/mods/misc/SwapMinePlace.java
@@ -20,6 +20,8 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.client.util.InputMappings.Input;
 import net.minecraftforge.client.event.InputEvent.KeyInputEvent;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -33,7 +35,7 @@ public class SwapMinePlace  extends ChildMod {
 		ClientRegistry.registerKeyBinding(mSwapKB);		
 	}
 
-	private static KeyBinding mSwapKB;
+	private static KeyBinding mSwapKB;	
 
 	@SubscribeEvent
 	public void onKeyInput(KeyInputEvent event) {	
@@ -54,5 +56,54 @@ public class SwapMinePlace  extends ChildMod {
 			ModUtils.sendPlayerMessage("Swapping mine and place keys");			
 			
 		}
+	}
+	
+	// This event is called by GuiIngameForge during each frame by
+	// GuiIngameForge.pre() and GuiIngameForce.post().
+	@SubscribeEvent
+	public void onRenderGameOverlayEvent(final RenderGameOverlayEvent.Post event) {
+
+		// We draw after the ExperienceBar has drawn.  The event raised by GuiIngameForge.pre()
+		// will return true from isCancelable.  If you call event.setCanceled(true) in
+		// that case, the portion of rendering which this event represents will be canceled.
+		// We want to draw *after* the experience bar is drawn, so we make sure isCancelable() returns
+		// false and that the eventType represents the ExperienceBar event.
+		if(event.isCancelable() || event.getType() != ElementType.EXPERIENCE)
+		{      
+			return;
+		}
+		
+		// If these are swapped, show a warning message
+		KeyBinding attackBinding = Minecraft.getInstance().gameSettings.keyBindAttack;
+		KeyBinding useBinding = Minecraft.getInstance().gameSettings.keyBindUseItem;
+		
+		if (attackBinding.isDefault() || useBinding.isDefault()) {
+			return;
+		}		
+		else {
+			Input attackDefault = attackBinding.getDefault();
+			Input useDefault = useBinding.getDefault();
+		
+			// if there's a straight-up swap, show message
+			if (attackBinding.getKey().getKeyCode() == useDefault.getKeyCode() &&
+					useBinding.getKey().getKeyCode() == attackDefault.getKeyCode()) {
+				Minecraft mc = Minecraft.getInstance();
+				int w = mc.mainWindow.getScaledWidth();
+				int h = mc.mainWindow.getScaledHeight();
+				
+				
+				String msg1 = "Mine / place";
+				int msg1width = mc.fontRenderer.getStringWidth(msg1);
+				String msg2 = "are swapped";
+				int msg2width = mc.fontRenderer.getStringWidth(msg2);
+				
+				int delta = (msg1width - msg2width)/2;
+			    
+			    mc.fontRenderer.drawStringWithShadow(msg1, w - msg2width - delta - 10 , h - 22, 0xffFFFFFF);
+			    mc.fontRenderer.drawStringWithShadow(msg2, w - msg2width - 10 , h - 12, 0xffFFFFFF);
+			    
+			}			
+		}
+		
 	}
 }
