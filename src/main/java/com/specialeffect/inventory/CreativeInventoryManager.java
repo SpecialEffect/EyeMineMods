bcb11255fbb0ac5e84482ae09158ee30f29682a8
Kirsty McNaught <kirsty.mcnaught@gmail.com>
2020-11-19 10:11:50 +0000

    Update copyright headers

--- src/main/java/com/specialeffect/inventory/CreativeInventoryManager.java
+++ src/main/java/com/specialeffect/inventory/CreativeInventoryManager.java
@@ -1,3 +1,14 @@
+/**
+ * Copyright (C) 2016-2020 Kirsty McNaught
+ * 
+ * Developed for SpecialEffect, www.specialeffect.org.uk
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ */
+
 package com.specialeffect.inventory;
 
 
--- src/main/java/com/specialeffect/mods/utils/KeyWatcher.java
+++ src/main/java/com/specialeffect/mods/utils/KeyWatcher.java
@@ -1,6 +1,7 @@
 /**
- * Copyright (C) 2020 Kirsty McNaught, SpecialEffect
- * www.specialeffect.org.uk
+ * Copyright (C) 2016-2020 Kirsty McNaught
+ * 
+ * Developed for SpecialEffect, www.specialeffect.org.uk
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
