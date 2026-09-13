a650e5a625b779a5cfa299b225765dfcf9ecba79
Mrbysco <Mrbysco@users.noreply.github.com>
2026-09-13 15:05:47 +0200

    Add back modmenu
    Move Cloth Config version into gradle properties

--- fabric/src/main/java/com/specialeffect/eyemine/config/EyeMineModMenuIntegration.java
+++ fabric/src/main/java/com/specialeffect/eyemine/config/EyeMineModMenuIntegration.java
@@ -1,20 +1,12 @@
 package com.specialeffect.eyemine.config;
 
-// TODO: Uncomment when ModMenu is available for MC 26.1.1
-// Requires: modImplementation "com.terraformersmc:modmenu:<version>" in fabric/build.gradle
-// And add "modmenu" entrypoint back to fabric.mod.json
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import me.shedaniel.autoconfig.AutoConfigClient;
 
-//import com.terraformersmc.modmenu.api.ConfigScreenFactory;
-//import com.terraformersmc.modmenu.api.ModMenuApi;
-//import me.shedaniel.autoconfig.AutoConfig;
-//
-//public class EyeMineModMenuIntegration implements ModMenuApi {
-//	@Override
-//	public ConfigScreenFactory<?> getModConfigScreenFactory() {
-//		return parent -> AutoConfig.getConfigScreen(EyeMineConfig.class, parent).get();
-//	}
-//}
-
-public class EyeMineModMenuIntegration {
-	// Stub class - ModMenu integration disabled until available for 26.1.1
+public class EyeMineModMenuIntegration implements ModMenuApi {
+	@Override
+	public ConfigScreenFactory<?> getModConfigScreenFactory() {
+		return parent -> AutoConfigClient.getConfigScreen(EyeMineConfig.class, parent).get();
+	}
 }
